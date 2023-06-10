9c891cf65a11ab246e8929a7f65cd0ff100840ae
arch <kamil@arch.pl>
2023-06-10 08:32:42 +0200

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,38 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link, graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import * as styles from "./styles/404.module.scss";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-const NotFoundPage = () => {
+const NotFoundPage = ({ data }) => {
+  console.log(data);
+  const image404 = getImage(data.image404);
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main>
+      <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          <GatsbyImage
+            image={image404}
+            className={styles.mainImage}
+          ></GatsbyImage>
+          <Link className={styles.button} to="/">
+            Przejdź
+          </Link>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head = () => <title>Not found</title>
+export const query = graphql`
+  query {
+    image404: file(relativePath: { eq: "404.webp" }) {
+      childImageSharp {
+        gatsbyImageData(quality: 100)
+      }
+    }
+  }
+`;
+
+export const Head = () => <title>Not found</title>;
